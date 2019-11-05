42046810ded640e8b5fc3b1bd5309f9f453fe08f
Chumachenko Roman <rom4ik730@gmail.com>
2019-11-05 18:55:05 +0200

    MainActivity renamed to NewsfeedActivity. Related changes were introduced to appropriate files.

--- app/src/main/java/com/legion1900/moxynews/NewsfeedActivity.kt
+++ app/src/main/java/com/legion1900/moxynews/NewsfeedActivity.kt
@@ -3,10 +3,10 @@ package com.legion1900.moxynews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NewsfeedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_newsfeed)
     }
 }
