bb950244fa77de6134924c007c30dced7d4dafbe
Chumachenko Roman <rom4ik730@gmail.com>
2019-11-06 17:21:29 +0200

    Created dialog factory for creation of error dialogs.

---
+++ app/src/main/java/com/legion1900/moxynews/utils/dialog/DialogFactory.kt
@@ -0,0 +1,24 @@
+package com.legion1900.moxynews.utils.dialog
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
+
+object DialogFactory {
+    fun buildErrorDialog(msg: Int, btnText: Int, callback: () -> Unit): DialogFragment {
+        return object : DialogFragment() {
+            override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+                val builder = AlertDialog.Builder(context)
+                builder.setMessage(msg)
+                    .setPositiveButton(btnText
+                    ) { dialog, which ->
+                        callback()
+                    }
+                return builder.create()
+            }
+        }
+    }
+}
