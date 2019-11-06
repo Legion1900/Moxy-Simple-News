989c135d4d2c4556dd4bc95c76614023b491eca8
Chumachenko Roman <rom4ik730@gmail.com>
2019-11-06 12:45:06 +0200

    Added ArticleView to NewsfeedContract. Updated Presenter contract so now  it is able to provide data to ArticleView.

--- app/src/main/java/com/legion1900/moxynews/contracts/NewsContract.kt
+++ app/src/main/java/com/legion1900/moxynews/contracts/NewsContract.kt
@@ -1,34 +1,68 @@
 package com.legion1900.moxynews.contracts
 
+import android.app.Activity
 import com.arellomobile.mvp.MvpView
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy
 import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
 object NewsContract {
     interface NewsfeedView : MvpView {
         @StateStrategyType(AddToEndSingleStrategy::class)
-        fun displayNewsfeed(authors: List<String>, titles: List<String>, dates: List<String>)
+        fun displayNewsfeed(articles: List<Article>)
         @StateStrategyType(SkipStrategy::class)
         fun setProgressBar(enabled: Boolean)
         /*
         * Launches activity to display article. Must be ignored by ViewState queue.
         * */
         @StateStrategyType(SkipStrategy::class)
-        fun openEntry(title: String, sourceName: String, description: String)
+        fun <T : Activity> openEntry(activity: Class<T>)
     }
 
-    interface ArticleModel {
+    interface ArticleView : MvpView {
+        @StateStrategyType(SingleStateStrategy::class)
+        fun displayArticle(article: Article)
+    }
+
+    interface Presenter {
+        fun updateNewsfeed(topic: String)
+        /*
+        * articleInd - index of article that was clicked in a List<Article>.
+        */
+        fun onArticleClick(articleInd: Int)
+        /*
+        * Provides article data for ArticleView.
+        * */
+        fun loadArticle(articleInd: Int)
+    }
+
+    interface NewsfeedModel {
+        val topic: String?
+        val news: List<Article>?
+        /*
+        * Asks to update cached in 'news' property articles.
+        * */
+        fun loadNews(topic: String)
+    }
+
+    /*
+    * Interfaces that describes data classes.
+    * */
+
+    interface Response {
+        val status: String
+        val code: String
+        val message: String
+        val articles: List<Article>
+    }
+
+    interface Article {
         val author: String
         val title: String
         val publishedAt: String
         val sourceName: String
         val urlToImage: String
         val description: String
-    }
-
-    interface Presenter {
-        fun update(topic: String)
-        fun onEntryClick(entryInd: Int)
     }
 }
