b1469ee615a08622df1998da7bb3dd9434d873e0
Chumachenko Roman <rom4ik730@gmail.com>
2019-11-06 17:22:00 +0200

    Implemented NewsfeedView.

--- app/src/main/java/com/legion1900/moxynews/views/NewsfeedActivity.kt
+++ app/src/main/java/com/legion1900/moxynews/views/NewsfeedActivity.kt
@@ -4,15 +4,22 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
-import com.legion1900.moxynews.views.adapters.ArticleAdapter
-import com.legion1900.moxynews.presenters.NewsPresenter
 import com.legion1900.moxynews.R
 import com.legion1900.moxynews.contracts.NewsContract
+import com.legion1900.moxynews.presenters.NewsPresenter
+import com.legion1900.moxynews.utils.dialog.DialogFactory
+import com.legion1900.moxynews.views.adapters.ArticleAdapter
 
 class NewsfeedActivity : MvpAppCompatActivity(), NewsContract.NewsfeedView {
+
+    companion object {
+        const val DIALOG_TAG = "com.legion1900.moxynews"
+    }
 
     @InjectPresenter
     lateinit var presenter: NewsPresenter
@@ -24,6 +31,14 @@ class NewsfeedActivity : MvpAppCompatActivity(), NewsContract.NewsfeedView {
                 rv.getChildAdapterPosition(view)
             )
         })
+    private lateinit var topics: Spinner
+
+    private val dialogCallback: () -> Unit = {
+        presenter.updateNewsfeed((topics.selectedItem as TextView).text.toString())
+        dialog.dismiss()
+    }
+    private val dialog =
+        DialogFactory.buildErrorDialog(R.string.msg_err, R.string.btn_positive, dialogCallback)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +58,8 @@ class NewsfeedActivity : MvpAppCompatActivity(), NewsContract.NewsfeedView {
     }
 
     override fun displayErrorDialog(visible: Boolean) {
-        TODO("create dialog fabric") //To change body of created functions use File | Settings | File Templates.
+        val manager = supportFragmentManager
+        dialog.show(manager, DIALOG_TAG)
     }
 
     override fun <T : Activity> openEntry(activity: Class<T>, article: NewsContract.Article) {
